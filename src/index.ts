510bcc551c0ce51172efb6a040aff54fdba97372
yicr <yicr@users.noreply.github.com>
2023-02-01 10:51:46 +0900

    feat!: first development.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,37 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import * as events from 'aws-cdk-lib/aws-events';
+import { Construct } from 'constructs';
+
+export interface EcsFargateTaskTerminationDetectionNotificationEventRuleProps {
+  readonly ruleName: string;
+  readonly description: string;
+  readonly clusterArn: string;
+  readonly targets?: events.IRuleTarget[];
+}
+
+export class EcsFargateTaskTerminationDetectionNotificationEventRule extends events.Rule {
+
+  constructor(scope: Construct, id: string, props: EcsFargateTaskTerminationDetectionNotificationEventRuleProps) {
+    super(scope, id, {
+      ruleName: props.ruleName,
+      description: props.description,
+      eventPattern: {
+        source: ['aws.ecs'],
+        detailType: ['ECS Task State Change'],
+        detail: {
+          clusterArn: props.clusterArn,
+          containers: {
+            exitCode: [
+              { 'anything-but': 0 },
+            ],
+          },
+          lastStatus: ['STOPPED'],
+          stoppedReason: [
+            {
+              'anything-but': { prefix: 'Scaling activity initiated by' },
+            },
+          ],
+        },
+      },
+    });
   }
 }
