1a13ee87c940ef7750d9524e5251be7b58d8bb83
yicr <yicr@users.noreply.github.com>
2023-02-01 12:22:44 +0900

    chore: package to npm access public.

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { awscdk } from 'projen';
+import { awscdk, javascript } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
@@ -8,6 +8,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   description: 'This an AWS ECS Fargate task termination detection Event Rule.',
   projenrcTs: true,
   repositoryUrl: 'https://github.com/yicr/ecs-fargate-task-termination-detection-event-rule.git',
+  npmAccess: javascript.NpmAccess.PUBLIC,
   // deps: [],                /* Runtime dependencies of this module. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
