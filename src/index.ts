0874a85814375963566420037e45793c40a38f19
yicr <yicr@users.noreply.github.com>
2024-07-01 15:07:40 +0900

    feat: add throw error when detail type

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import * as events from 'aws-cdk-lib/aws-events';
 import { Construct } from 'constructs';
 
-export interface EcsFargateTaskTerminationDetectionEventRuleProps {
-  readonly ruleName?: string;
-  readonly description?: string;
+export interface EcsFargateTaskTerminationDetectionEventRuleProps extends events.RuleProps {
   readonly clusterArn: string;
 }
 
@@ -11,26 +9,30 @@ export class EcsFargateTaskTerminationDetectionEventRule extends events.Rule {
 
   constructor(scope: Construct, id: string, props: EcsFargateTaskTerminationDetectionEventRuleProps) {
     super(scope, id, {
-      ruleName: props.ruleName,
-      description: props.description,
-      eventPattern: {
-        source: ['aws.ecs'],
-        detailType: ['ECS Task State Change'],
-        detail: {
-          clusterArn: props.clusterArn,
-          containers: {
-            exitCode: [
-              { 'anything-but': 0 },
+      ...props,
+      eventPattern: (() => {
+        if (props.eventPattern) {
+          throw new Error('InvalidArgumentException: The specified argument eventPattern is predefined and should not be changed.');
+        }
+        return {
+          source: ['aws.ecs'],
+          detailType: ['ECS Task State Change'],
+          detail: {
+            clusterArn: props.clusterArn,
+            containers: {
+              exitCode: [
+                { 'anything-but': 0 },
+              ],
+            },
+            lastStatus: ['STOPPED'],
+            stoppedReason: [
+              {
+                'anything-but': { prefix: 'Scaling activity initiated by' },
+              },
             ],
           },
-          lastStatus: ['STOPPED'],
-          stoppedReason: [
-            {
-              'anything-but': { prefix: 'Scaling activity initiated by' },
-            },
-          ],
-        },
-      },
+        };
+      })(),
     });
   }
 }
