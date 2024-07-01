da35df6b270c612601136a319271dad32fc1708b
yicr <yicr@users.noreply.github.com>
2024-07-01 15:31:18 +0900

    chore(test): rename test file, with add error test

---
+++ test/rule.error.test.ts
@@ -0,0 +1,28 @@
+import { App, Stack } from 'aws-cdk-lib';
+import { EcsFargateTaskTerminationDetectionEventRule } from '../src';
+
+describe('EcsFargateTaskTerminationDetectionNotificationEventRule Testing', () => {
+  const app = new App();
+  const stack = new Stack(app, 'TestingStack', {
+    env: {
+      account: '123456789012',
+      region: 'us-east-1',
+    },
+  });
+
+  const clusterArn = 'arn:aws:ecs:us-east-1:123456789012:cluster/example-app-cluster';
+
+  it('Should Error', () => {
+    expect(() => {
+      new EcsFargateTaskTerminationDetectionEventRule(stack, 'EcsFargateTaskTerminationDetectionEventRule', {
+        clusterArn,
+        ruleName: 'codepipeline-state-change-detection-event-rule',
+        eventPattern: {
+          source: ['aws.ecs'],
+          detailType: ['ECS Task State Change'],
+        },
+      });
+    }).toThrow(/InvalidArgumentException: The specified argument eventPattern is predefined and should not be changed./);
+  });
+
+});
